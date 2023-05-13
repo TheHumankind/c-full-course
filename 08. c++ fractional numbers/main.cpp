63890ef3ccb497c7be1fbbd71413e24135e431b6
Hurin Vlad <longsno@gmail.com>
2023-05-13 22:56:20 +0300

    lesson 8 comments update

--- 08. c++ fractional numbers/main.cpp
+++ 08. c++ fractional numbers/main.cpp
@@ -2,16 +2,16 @@
 #include <iomanip>
 
 int main(){
-    float num_01 {1.12232323232323232323232f};
-    double num_02 {1.12122313123123123123123213};
-    long double num_03 {1.1212231312312312312312321L};
+    float num_01 {1.12232323232323232323232f}; // 7 digits
+    double num_02 {1.12122313123123123123123213}; // 15 digits
+    long double num_03 {1.1212231312312312312312321L}; // > double
 
-    std::cout << "size of float: " << sizeof(float) << std::endl;
-    std::cout << "size of float: " << sizeof(double) << std::endl;
-    std::cout << "size of float: " << sizeof(long double) << std::endl;
+    std::cout << "size of float: " << sizeof(float) << std::endl; // 4 bytes
+    std::cout << "size of float: " << sizeof(double) << std::endl; // 8 bytes
+    std::cout << "size of float: " << sizeof(long double) << std::endl; // 12 bytes
 
 
-    std::cout << std::setprecision(20);
+    std::cout << std::setprecision(20); // count of numbers after dot
     std::cout << "num 1: " << num_01 << std::endl;
     std::cout << "num 2: " << num_02 << std::endl;
     std::cout << "num 3: " << num_03 << std::endl;
