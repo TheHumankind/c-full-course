14f1211f49880eaa719f7762edf7c8d0946e2358
Hurin Vlad <longsno@gmail.com>
2023-05-16 19:58:15 +0300

    c++ basic operations lesson

--- chapter 2 operations on data/01. c++ operations on data/main.cpp
+++ chapter 2 operations on data/01. c++ operations on data/main.cpp
@@ -1,13 +1,13 @@
 #include <iostream>
 
 int main() {
-    int num_1 {10};
+    int num_1 {8};
     int num_2 {5};
 
     std::cout << "a + b = " << num_1 + num_2 << std::endl;
-    std::cout << "a - b = " << num_1 + num_2 << std::endl;
-    std::cout << "a * b = " << num_1 + num_2 << std::endl;
-    std::cout << "a / b = " << num_1 + num_2 << std::endl;
+    std::cout << "a - b = " << num_1 - num_2 << std::endl;
+    std::cout << "a * b = " << num_1 * num_2 << std::endl;
+    std::cout << "a / b = " << num_1 / num_2 << std::endl;
     std::cout << "a % b = " << num_1 % num_2 << std::endl;
 
     return 0;
