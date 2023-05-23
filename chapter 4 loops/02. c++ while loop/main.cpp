cba85e9c9aeb846fd06a232ffd0ca9164c7b1788
Hurin Vlad <longsno@gmail.com>
2023-05-23 18:30:12 +0300

    loops

---
+++ chapter 4 loops/02. c++ while loop/main.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+
+int main(){
+
+    size_t COUNT {10};
+
+    size_t i {};
+
+    while (i < COUNT)
+    {
+        std::cout << "C++" << std::endl;
+        ++i;
+    }
+}
---
+++ chapter 4 loops/03. c++ do while loop/main.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+
+int main(){
+
+    const size_t COUNT {10};
+
+    size_t i {0};
+
+    do
+    {
+       std::cout << "c++" << std::endl;
+       ++i;
+    } while (i < COUNT);
+    
+}
